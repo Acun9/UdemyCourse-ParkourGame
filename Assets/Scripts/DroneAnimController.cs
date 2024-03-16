92d95ce17f8bc9e0168adcbdfe290cd2944cd5f4
Acun9 <83479146+Acun9@users.noreply.github.com>
2024-03-16 21:47:08 +0300

    Drone (Enemy) added

---
+++ Assets/Scripts/DroneAnimController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneAnimController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown("k"))
+        {
+            anim.Play("ShutDown");
+        }
+
+        if (Input.GetKeyDown("j"))
+        {
+            anim.Play("WakeUp");
+        }
+
+        if (Input.GetKeyDown("l"))
+        {
+            anim.Play("Destroyed");
+        }
+
+    }
+}
